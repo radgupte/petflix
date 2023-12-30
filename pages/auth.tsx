f9437ae2c0330c0426daab0f8b5f7ccad4c8fada
Radhika Gupte <gupter@oregonstate.edu>
2023-12-29 16:27:15 -0800

    created a user

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -28,7 +28,7 @@ const Auth = () => {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [email, name, password]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -66,7 +66,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === 'login' ? 'Login' : 'Sign Up'}
             </button>
             <p className="text-neutral-500 mt-12">
