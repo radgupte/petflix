1dcb5258b463c79ef6ec24d2496eb732bafd3116
Radhika Gupte <gupter@oregonstate.edu>
2023-12-30 16:38:28 -0800

    added a profile page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,13 +6,11 @@ import { useCallback, useState } from 'react';
 import axios from 'axios';
 import Input from '../components/input';
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
-  const router = useRouter();
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -29,11 +27,8 @@ const Auth = () => {
       await signIn('credentials', {
         email,
         password,
-        redirect: false,
-        callbackUrl: '/',
+        callbackUrl: '/profiles',
       });
-
-      router.push('/');
     } catch (error) {
       console.log(error);
     }
@@ -97,7 +92,7 @@ const Auth = () => {
 
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn('google', { callbackUrl: '/' })}
+                onClick={() => signIn('google', { callbackUrl: '/profiles' })}
                 className="
                 w-10
                 h-10
@@ -114,7 +109,7 @@ const Auth = () => {
                 <FcGoogle size={30} />
               </div>
               <div
-                onClick={() => signIn('github', { callbackUrl: '/' })}
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
                 className="
                 w-10
                 h-10
