afa263ba11b1400b8c3ecc6deca7b14f2c0b2c4f
Radhika Gupte <gupter@oregonstate.edu>
2024-01-02 16:28:29 -0800

    created movie cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 import Navbar from '@/components/navbar';
 import Billboard from '@/components/billboard';
+import MovieList from '@/components/movieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,10 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
