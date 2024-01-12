c87df89a87d4982129f6997cdaf404843f75c478
Radhika Gupte <gupter@oregonstate.edu>
2024-01-11 18:00:44 -0800

    changed Petflix to Netflix temporarily

--- components/accountMenu.tsx
+++ components/accountMenu.tsx
@@ -30,7 +30,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
           onClick={() => signOut()}
           className="px-3 text-center text-white text-sm hover:underline"
         >
-          Sign out of Petflix
+          Sign out of Netflix
         </div>
       </div>
     </div>
